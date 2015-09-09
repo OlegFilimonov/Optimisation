afa21719033608d152c4c642a8da52ffbb7e3bef
Oleg Filimonov <themraction@gmail.com>
2015-09-09 22:18:16 +0300

    added one dimentional method class

---
+++ Optimisation/OneDimentionalOptimisationMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Optimisation
+{
+    public abstract class OneDimentionalOptimisationMethod
+    {
+        private List<double> startingPoints = new List<double>(2);
+
+        public delegate double function(double x);
+
+        private List<double> sven(function f)
+        {
+            if (f == null) throw new ArgumentNullException(nameof(f));
+            double a, b;
+            
+            List<double> ans = new List<double>(2);
+            //ans.Add(a);
+            //ans.Add(b);
+            return ans;
+        }
+
+        protected OneDimentionalOptimisationMethod(function f)
+        {
+            startingPoints = sven(f);
+        }
+    }
+}
--- Optimisation/Program.cs
+++ Optimisation/Program.cs
@@ -6,6 +6,25 @@ using System.Threading.Tasks;
 
 namespace Optimisation
 {
+    //   Класс типичных функций для тестирования
+    //   TODO: создать класс теста и переместить данный класс туда вместе с методами
+    class TestingFunctions
+    {
+        // Функция y = x^2
+        double f1(double x)
+        {
+            return Math.Pow(x, 2);
+        }
+
+        // Функция y = x^3 + 5x^2 - 5x + 65
+        double f2(double x)
+        {
+            return Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 5 * x + 65;
+           
+        }
+    }
+
+    //Основной класс программы
     class Program
     {
         static void Main(string[] args) => Console.Write("Hello, universe!");
